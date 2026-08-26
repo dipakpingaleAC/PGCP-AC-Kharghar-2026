46f551f04ffdd719285ef116fe3bc736b08c4e74
Dipak  Pingale <dipak.pingale.cmaug26@gmail.com>
2026-08-26 22:33:24 +0530

    Add files via upload

---
+++ C++/Day 1/Sum.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+   int a;
+   int b;
+	cout<<"Enter two numbers:";
+	cin>>a;
+	cin>>b;
+	int c=a+b;
+	cout<<"Addition is:" <<c;
+   return 0;
+}
