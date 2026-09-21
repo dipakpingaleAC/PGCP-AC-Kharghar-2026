64328e1196746b6a77ca58f9e9325da54cf04d79
Dipak  Pingale <dipak.pingale.cmaug26@gmail.com>
2026-09-21 22:53:25 +0530

    Add files via upload

---
+++ OOP Java/ForLoops/EvenNumber.java
@@ -0,0 +1,17 @@
+
+public class EvenNumber {
+
+	public static void main(String[] args) {
+		System.out.println("Even numbers between 1 to 500 are:");
+		for (int i=1;i<=500;i++) {
+			
+			if(i%2==0) {
+				System.out.println(i +"  ");
+			}
+		}
+		
+
+
+	}
+
+}
