64a556aef3f094cc815e9d04c172ce4b58bca4cb
Dipak  Pingale <dipak.pingale.cmaug26@gmail.com>
2026-09-21 22:52:43 +0530

    Add files via upload

---
+++ OOP Java/ForLoops/oddNumbers.java
@@ -0,0 +1,16 @@
+
+public class oddNumbers {
+
+	public static void main(String[] args) {
+		System.out.println("Odd numbers between 1 to 1000 are:");
+		for (int i=1;i<=1000;i++) {
+			
+			if(i%2!=0) {
+				System.out.println(i +"  ");
+			}
+		}
+		
+
+	}
+
+}
